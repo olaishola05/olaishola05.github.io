919c0ab6ed4eb3a39c529e5f5960009d49683c38
Ola Ishola <45001916+netman5@users.noreply.github.com>
2021-12-13 17:43:43 +0100

    Fix error with styling

--- app.js
+++ app.js
@@ -5,11 +5,17 @@ const toggleBtn = document.querySelector('.toggle-btn');
 const toggleSideMenu = false;
 const toggleCloseBtn = false;
 
-
 function toggleNav() {
-    if(toggleSideMenu===false){
-        navMenu.style.visibilty = 'visible'
-    }
+  if (toggleSideMenu === false) {
+    navMenu.style.visibility = 'visible';
+  }
+}
+
+function toggleClose() {
+  if (toggleCloseBtn === false) {
+    navMenu.style.visibility = 'hidden';
+  }
 }
 
 hamburger.addEventListener('click', toggleNav);
+toggleBtn.addEventListener('click', toggleClose);
