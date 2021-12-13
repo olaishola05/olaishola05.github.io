21a589d1187e98c7d49ee3e50596aa3d1a8cdb3b
Ola Ishola <45001916+netman5@users.noreply.github.com>
2021-12-13 18:55:01 +0100

    Add function to close nav when link is clicked && update style.css

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const hamburger = document.querySelector('#menu');
 const navMenu = document.querySelector('.mobile-menu');
 const toggleBtn = document.querySelector('.toggle-btn');
+const navItems = document.querySelectorAll('.mobile-menu ul li');
 
 const toggleSideMenu = false;
 const toggleCloseBtn = false;
@@ -8,6 +9,7 @@ const toggleCloseBtn = false;
 function toggleNav() {
   if (toggleSideMenu === false) {
     navMenu.style.visibility = 'visible';
+    toggleBtn.style.fontSize = '30px';
   }
 }
 
@@ -16,6 +18,16 @@ function toggleClose() {
     navMenu.style.visibility = 'hidden';
   }
 }
+
+function linksOnClick() {
+  navItems.forEach((link) => {
+    link.addEventListener('click', () => {
+      navMenu.style.visibility = 'hidden';
+    });
+  });
+}
+
+linksOnClick();
 
 hamburger.addEventListener('click', toggleNav);
 toggleBtn.addEventListener('click', toggleClose);
