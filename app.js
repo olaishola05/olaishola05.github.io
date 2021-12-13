2275108e7f02a3e244a43077f6c03329e068ba34
Ola Ishola <45001916+netman5@users.noreply.github.com>
2021-12-13 16:01:28 +0100

    Create menu for mobile, add variables in js and update styles

---
+++ app.js
@@ -0,0 +1,7 @@
+const hamburger = document.querySelector('#menu');
+const navMenu = document.querySelector('.mobile-menu');
+const toggleBtn = document.querySelector('.toggle-btn');
+
+function toggleNav() {}
+
+hamburger.addEventListener('click', toggleNav);
