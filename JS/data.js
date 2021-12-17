2c5d56422e39b920ebe4114dd21b6ec3aac6d8ed
Ola Ishola <45001916+netman5@users.noreply.github.com>
2021-12-17 20:25:14 +0100

    Rename utils file name to JS for clearity

---
+++ JS/data.js
@@ -0,0 +1,90 @@
+const randomID = () => Math.floor(Math.random() * 101);
+
+// eslint-disable-next-line import/prefer-default-export
+export const projects = [
+  {
+    id: randomID(),
+    name: 'Tonic',
+    mobileName: 'Tonic',
+    detailsMobile: ['CANOPY', 'Backend End Dev', 2015],
+    details: ['CANOPY', 'Backend End Dev', 2015],
+    description:
+      'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    largeDescription:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
+    mobileDescription:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
+    stacks: ['html', 'css', 'javascript'],
+    stacksPopup: ['html', 'css', 'javaScript', 'github', 'Ruby', 'Bootstrap'],
+    imageDesktop: './images/Snapshoot-Portfolio-1.svg',
+    imageMobile: './images/SnapshootPortfolio.svg',
+    popupImageDesktop: './images/Snapshoot-Portfolio-1.svg',
+    popupImageMobile: './images/SnapshootPortfolio.svg',
+    liveUrl: '',
+    sourceUrl: '',
+  },
+  {
+    id: randomID(),
+    name: 'Multi-Post Stories',
+    mobileName: 'Multi-Post Stories',
+    detailsMobile: ['CANOPY', 'Backend End Dev', 2015],
+    details: ['FACEBOOK', 'Fullstack Dev', 2015],
+    description:
+      'Experimental content creation feature that allows users to add to an existing story over the course of a day without spamming their friends.',
+    largeDescription:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
+    mobileDescription:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
+
+    stacks: ['html', 'css', 'javascript'],
+    stacksPopup: ['html', 'Ruby on rails', 'css', 'javaScript'],
+    imageDesktop: './images/art-printing.svg',
+    imageMobile: './images/avail.svg',
+    popupImageDesktop: './images/art-printing.svg',
+    popupImageMobile: './images/avail.svg',
+    liveUrl: '',
+    sourceUrl: '',
+  },
+  {
+    id: randomID(),
+    name: 'Facebook 360',
+    mobileName: 'Tonic',
+    detailsMobile: ['CANOPY', 'Backend End Dev', 2015],
+    details: ['FACEBOOK', 'Fullstack Dev', 2015],
+    description:
+      "Exploring the future of media in Facebook's first Virtual Reality app; a place to discover and enjoy 360 photos and videos on Gear VR.",
+    largeDescription:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
+    mobileDescription:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
+    stacks: ['html', 'css', 'javascript'],
+    stacksPopup: ['html', 'Ruby on rails', 'css', 'javaScript'],
+    imageDesktop: './images/SnapshootPortfolio.svg',
+    imageMobile: './images/gymfit.svg',
+    popupImageDesktop: './images/SnapshootPortfolio.svg',
+    popupImageMobile: './images/gymfit.svg',
+    liveUrl: '',
+    sourceUrl: '',
+  },
+  {
+    id: randomID(),
+    name: 'Uber Navigation',
+    details: ['Uber', 'Lead Developer', 2018],
+    mobileName: 'Multi-Post Stories',
+    detailsMobile: ['CANOPY', 'Backend End Dev', 2015],
+    description:
+      'A smart assistant to make driving more safe, efficient, and fun by unlocking your most expensive computer: your car.',
+    largeDescription:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
+    mobileDescription:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
+    stacks: ['html', 'css', 'javaScript'],
+    stacksPopup: ['html', 'Ruby on rails', 'css', 'javaScript'],
+    imageDesktop: './images/avail.svg',
+    imageMobile: './images/art-printing.svg',
+    popupImageDesktop: './images/avail.svg',
+    popupImageMobile: './images/art-printing.svg',
+    liveUrl: '',
+    sourceUrl: '',
+  },
+];
